0dd8a8970b13586c768b117d27f20ab41dffc50c
jcacc22 <jcacc22@users.noreply.github.com>
2019-11-27 18:50:05 +0100

    I dont remember what i added
    No idea what i did, im tired but i know i need to commit.

---
+++ PacMan/PacMan/Assets/Scripts/pacMover.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pacMover : MonoBehaviour
+{
+    public float speed = 0.4f;
+    Vector2 dest = Vector2.zero;
+
+    void Start()
+    {
+        dest = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        // Move closer to Destination
+        Vector2 p = Vector2.MoveTowards(transform.position, dest, speed);
+        GetComponent<Rigidbody2D>().MovePosition(p);
+    }
+
+    bool valid(Vector2 dir)
+    {
+        // Cast Line from 'next to Pac-Man' to 'Pac-Man'
+        Vector2 pos = transform.position;
+        RaycastHit2D hit = Physics2D.Linecast(pos + dir, pos);
+        return (hit.collider == GetComponent<Collider2D>());
+    }
+}
