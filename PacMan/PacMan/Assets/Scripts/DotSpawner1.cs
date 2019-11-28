967c2a47d6fe4c533fa2153feab3ca44211f838c
jcacc22 <jcacc22@users.noreply.github.com>
2019-11-28 11:40:15 +0100

    Put in pellets and stuff

---
+++ PacMan/PacMan/Assets/Scripts/DotSpawner1.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DotSpawner1 : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D co)
+    {
+        if (co.name == "pacMan")
+            Destroy(gameObject);
+    }
+}
