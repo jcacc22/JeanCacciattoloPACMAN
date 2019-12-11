666d5ec267ec486142278e2d0027f8e5f78f447d
jcacc22 <56068134+jcacc22@users.noreply.github.com>
2019-12-11 11:40:14 +0100

    Practical 1 task 2

---
+++ Practical 1 Task 2/BilliardGame_Practical_part2/Assets/Scripts/ColouredBall.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColouredBall : MonoBehaviour
+{
+
+
+    void DestroyBallWhenItHitsHole(Collision collision)
+    {
+        Destroy(collision.collider.gameObject);
+        Destroy(gameObject);
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    
+    
+}
+
+
---
+++ Practical 1 Task 2/BilliardGame_Practical_part2/Assets/Scripts/WhiteBall.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhiteBall : MonoBehaviour
+{
+ 
+
+    bool hasStarted = false;
+    int chances = 5;
+
+
+    void Start()
+    {
+
+
+    }
+
+    void Update()
+    {
+        if (!hasStarted) 
+        {
+            LaunchBallOnClick();
+        }
+    }
+
+    private void LaunchBallOnClick()
+    {
+            if (Input.GetMouseButtonDown(0))
+            {
+
+
+                var x = Random.Range(-1, 2);
+                var y = Random.Range(15, 21);
+                hasStarted = true;
+                GetComponent<Rigidbody2D>().velocity = new Vector2(x, y);
+
+            }
+            
+        }
+    }
